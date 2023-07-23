a699a770869b0dc8e968e7562b516391011753b2
Aybars Duran <aybarsduran@outlook.com>
2023-07-23 19:37:45 +0300

    added database for objects

--- Assets/Script/PlacementSystem.cs
+++ Assets/Script/PlacementSystem.cs
@@ -9,8 +9,51 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField] private GameObject cellIndicator;
     [SerializeField] private Grid grid;
 
+    [SerializeField] private ObjectsDatabaseSO database;
+    [SerializeField] private int selectedObjectIndex = -1;
+    [SerializeField] private GameObject gridVisualization;
+    private void Start()
+    {
+        StopPlacement();
+    }
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        selectedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID);
+        if(selectedObjectIndex < 0)
+        {
+            Debug.LogError($"No ID found {ID}");
+            return;
+        }
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+    public void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+    public void PlaceStructure()
+    {
+        if(inputManager.IsPointerOverUI())
+        {
+            return;
+        }
+        Vector3 mousePosition = inputManager.GetSelectedMousePosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject gameObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
+        gameObject.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+
     private void Update()
     {
+        if (selectedObjectIndex < 0) { return; }
         Vector3 mousePosition = inputManager.GetSelectedMousePosition();
         Vector3Int gridPosition= grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
