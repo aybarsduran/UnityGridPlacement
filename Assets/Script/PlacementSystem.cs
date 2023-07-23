deb637fe92ee816e20a7be3ea1b49ba5ca4ddaf3
Aybars Duran <aybarsduran@outlook.com>
2023-07-23 04:04:08 +0300

    added grid selection

--- Assets/Script/PlacementSystem.cs
+++ Assets/Script/PlacementSystem.cs
@@ -6,11 +6,15 @@ public class PlacementSystem : MonoBehaviour
 {
     [SerializeField] private GameObject mouseIndicator;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private GameObject cellIndicator;
+    [SerializeField] private Grid grid;
 
     private void Update()
     {
         Vector3 mousePosition = inputManager.GetSelectedMousePosition();
+        Vector3Int gridPosition= grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
 
     }
 
